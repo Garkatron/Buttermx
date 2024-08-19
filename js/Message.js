46d1d589589b463fee332314b3733b27a89559d1
Garkatron <145294535+Garkatron@users.noreply.github.com>
2024-08-19 12:23:10 +0200

    1.0.8

--- js/Message.js
+++ js/Message.js
@@ -17,6 +17,6 @@ export class Message {
 }
 
 export let messages = {
-    felizcumpleaños: new Message("🎉Feliz Cumpleaños🎉","Tengo dudas de si llegaras a leer esto 🫤.\nLo pondre en github, asi puedes ver el contenido y asegurarte de que esto no es malicioso.\nEspero que esto sea algo minimamente original o entretenido."),
+    felizcumpleaños: new Message("🎉Feliz Cumpleaños🎉","Tengo dudas de si llegaras a leer esto 🫤.\nLo pondre en github, asi puedes ver el contenido.\nEspero que esto sea algo minimamente original o entretenido.\nAl final lo puse en vercel porque en github pages no funcionaba."),
     visteelrepo: new Message("Si ves esto es porque abriste el repo de github","No me imagine que lo llegaras a abrir.")
 }
